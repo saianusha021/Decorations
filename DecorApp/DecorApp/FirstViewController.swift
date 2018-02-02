5216b87eb44c9f6a3fc753dbef9d9df518cba4bb
Anusha <saianush021@gmail.com>
2018-02-01 21:55:41 -0500

    Started with the Home Page

--- DecorApp/DecorApp/FirstViewController.swift
+++ DecorApp/DecorApp/FirstViewController.swift
@@ -12,6 +12,7 @@ class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "Home"
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +21,6 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+ 
 }
 
